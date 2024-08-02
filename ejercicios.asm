11f776e2de895562ff35c5878ff2359fd4abd150
NicolasQ455359 <nicolas.quintero@upb.edu.co>
2024-08-02 07:24:55 -0500

    Update ejercicios.asm
    Añadir ejercicio de suma y resta y su explicación

--- ejercicios.asm
+++ ejercicios.asm
@@ -51,3 +51,60 @@ En este ejercicio, se implementa una suma acumulativa mediante el uso de bucles 
    - Cuando R1 llega a 0, el programa salta a la etiqueta (END), finalizando el bucle.
    - Se utiliza un bucle infinito para detener el programa.
 
+@0
+D=M           
+@R0
+M=D           
+
+@1
+D=M           
+@R1
+M=D           
+
+@2
+D=M           
+@R2
+M=D           
+
+@R0
+D=M           
+@R1
+D=D+M         
+
+@R2
+D=D-M         
+
+@LOOP
+D;JGE         
+
+@3
+M=0           
+@END
+0;JMP         
+
+(LOOP)
+@3
+M=D           
+
+(END)
+@END
+0;JMP
+
+// Explicación
+/*
+En este ejercicio, se realizan las siguientes operaciones:
+
+1. Inicialización:
+   - Los valores desde las posiciones de memoria 0, 1 y 2 se cargan en los registros temporales R0, R1 y R2 respectivamente.
+
+2. Suma y resta:
+   - Se suman los valores de R0 y R1 y se almacena el resultado en D.
+   - Luego, se resta el valor de R2 del resultado previo almacenado en D.
+
+3. Condicional:
+   - Se verifica si el resultado almacenado en D es mayor o igual a 0. Si es así, salta a la etiqueta LOOP.
+
+4. Resultado:
+   - Si el resultado es negativo, se almacena 0 en la posición de memoria 3 y se salta al final.
+   - Si el resultado es positivo, se almacena el valor de D en la posición de memoria 3.
+
